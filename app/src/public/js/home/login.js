0ee43a3cded1b03477f640dd9869e204688d04a8
SuBrooo <clark9810@naver.com>
2021-12-25 20:27:26 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 "use strict";
-console.log("hello");
-console.log("bye");
+
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
+
+console.log(id);
+/*defer,async 옵션을 <script src="/js/home/login.js" defer></script>(defer를 사용하자!!)
+이런식으로 넣어주어야함 그래야만 login.js에서 document.querySelector("#id");으로 #id를 가져오기 전에
+ console.log(id)가 먼저 실행 됨을 막아 줄수 있음.*/
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+}
